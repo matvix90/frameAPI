a5ccd3c5d5a5c66d64590b0dbfd6d11f1a9e6220
matvix90 <matvix90@gmail.com>
2025-01-03 19:38:54 +0100

    add new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="frame-API",
-    version="0.1.0",
+    version="0.1.2",
     description="A lightweight Python web framework.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
